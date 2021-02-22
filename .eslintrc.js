f1ed3316e54b64f3af68bf7666a710dd1c1376dc
pacexy <pacexy@gmail.com>
2021-02-22 16:17:36 +0800

    Use Hitoka

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,13 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
+    './node_modules/hitoka/.eslintrc.js',
     'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
